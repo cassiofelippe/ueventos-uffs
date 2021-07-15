514ba46265702c38756c95031e4fd2da73509493
Cassio Felippe <cassioluis12@gmail.com>
2021-07-14 21:36:30 -0300

    visualizando eventos existentes

--- src/components/EventosIndex.vue
+++ src/components/EventosIndex.vue
@@ -4,9 +4,17 @@
       <b-col>
         <label>Eventos UFFS</label>
       </b-col>
+      
       <b-col>
         <b-button @click="novoEvento">+ Criar evento</b-button>
       </b-col>
+
+      <ul>
+        <label>Eventos existentes</label>
+        <li v-for="e, i in eventos" v-bind:key="i">
+          {{e.nome}} -> {{e.data}}
+        </li>
+      </ul>
     </b-container>
   </div>
 </template>
@@ -18,7 +26,12 @@
     props: [],
 
     data() {
-      return {};
+      return {
+        eventos: [
+           {nome: 'tabajara', data: '15/05/1998'},
+           {nome: 'showpanas', data: '15/05/2021'},
+        ]
+      };
     },
 
     methods: {
