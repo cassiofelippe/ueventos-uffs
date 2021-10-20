a26707ed4d285df84885629873623a05ea1017db
CassioFelippe <cassioluis12@gmail.com>
2021-10-20 19:14:42 -0300

    added style

--- src/components/Eventos.vue
+++ src/components/Eventos.vue
@@ -84,6 +84,8 @@
       <b-col cols="12" sm="12" md="12" lg="12" class="text-center" v-if="model.idEvento && !disabled">
         <b-button @click="salvar" variant="success">Salvar</b-button>
       </b-col>
+      
+      <b-col cols="12">&nbsp;</b-col>
     </b-row>
   </b-form>  
 </template>
@@ -156,4 +158,55 @@
 </script>
 
 <style scoped>
+    .form{
+      font-weight:bold;
+      background-color: white;
+      border: 4px solid;
+      border-color: green;
+      margin: 0 auto;
+      width: 100%; /* Valor da Largura */
+      text-align: center;
+      align-items: center;
+      color: green;
+      background-color: white;
+      padding-top: 1em;
+      padding-left: 1em;
+      padding-right: 1em;
+    }
+    label{
+      text-align: center;
+      Width: 100%;
+    }
+    .eventos-nome{
+      align-items: all;
+    }
+    button{
+      background-color: #b3b3b3;
+      width:150px;
+      height:40px;
+      font-weight:bold;
+      border-radius: 5px;
+      border-color: green;
+      margin: 110 px;
+    }
+    h1{
+      font-weight:bold;
+      color: green;
+    }
+
+    #evento-area-conhecimento, #evento-modalidade, input[type="text"],input[type="email"],input[type="tel"], input[type="date"], input[type="file"],
+    input[type="password"],input[type="number"],input[type="time"],input[type="button"] select, textarea{
+      width: 100%;
+      border: 5px solid ;
+      border-color: green;
+      margin: 10px 0px;
+      border-radius: 5px;
+      padding: 5px 20px;
+      background-color:#b3b3b3;
+      color: black;
+      font-family: monospace;
+      left:2px;
+      margin: 0 auto;
+      position: relative;
+    }
 </style>
